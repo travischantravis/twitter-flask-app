24ebedf21c0bf4494ea853f6614df705d6a61f48
travischantravis <chancheukyin4463@gmail.com>
2020-09-10 21:22:06 -0700

    set up api key

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import tweepy
 
+# Set-up
 app = Flask(__name__)
+app.config.from_pyfile('envvar.py')
+TWITTER_API_KEY = app.config.get("TWITTER_API_KEY")
+TWITTER_API_KEY_SECRET = app.config.get("TWITTER_API_KEY_SECRET")
 
 
-@app.route('/')
-def hello():
-    return {"msg": 'Hello, World!'}
+# Home route
+@app.route("/")
+@app.route("/hello/<name>")
+def home_page(name="Travis"):
+    return render_template('index.html', name=TWITTER_API_KEY_SECRET)
